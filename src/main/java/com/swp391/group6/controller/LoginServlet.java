9cc27d7d51c0a7dfd10518f4736fd31452689e77
vuongdd <vuongddhe186713@fpt.edu.vn>
2026-01-20 15:59:13 +0700

    logout

--- src/main/java/com/swp391/group6/controller/LoginServlet.java
+++ src/main/java/com/swp391/group6/controller/LoginServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package com.swp391.group6.controller;
 
 import com.swp391.group6.dao.UserDao;
@@ -16,10 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-/**
- *
- * @author Admin
- */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
 public class LoginServlet extends HttpServlet {
     

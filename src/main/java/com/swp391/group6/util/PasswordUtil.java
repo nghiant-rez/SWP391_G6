6ad63aa3ba0d5e66a7de203049c224e694bc9dc6
Nguyen Trung Nghia <nghianthe150495@fpt.edu.vn>
2026-01-21 23:54:12 +0700

    Update PasswordUtil.java

--- src/main/java/com/swp391/group6/util/PasswordUtil.java
+++ src/main/java/com/swp391/group6/util/PasswordUtil.java
@@ -11,27 +11,27 @@ import org.mindrot.jbcrypt.BCrypt;
  * @author Admin
  */
 public class PasswordUtil {
-    
+
     // mã hóa pass
-    public static String hashPassword(String plainPassword){
+    public static String hashPassword(String plainPassword) {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
-    
+
     // kiểm tra password có khớp với hash không.
-    public static boolean checkPassword(String plainPassword, String hashPassword){
-        try{
+    public static boolean checkPassword(String plainPassword, String hashPassword) {
+        try {
             return BCrypt.checkpw(plainPassword, hashPassword);
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("Lỗi: chuỗi hash không đúng định dạnh BCrypt!");
             return false;
         }
     }
-    
+
     //Test main
     public static void main(String[] args) {
         String plainPassword = "123456";
         String hashed = hashPassword(plainPassword);
-        
+
         System.out.println("Plain Password: " + plainPassword);
         System.out.println("Hashed Password : " + hashed);
         System.out.println("Password Match: " + checkPassword(plainPassword, hashed));

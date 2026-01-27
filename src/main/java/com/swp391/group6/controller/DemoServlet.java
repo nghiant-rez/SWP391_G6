62b8485dd5062e33ce5adf6a4d62cdfd071ca559
Nguyen Trung Nghia <nghianthe150495@fpt.edu.vn>
2026-01-27 16:28:42 +0700

    edit DemoServlet

--- src/main/java/com/swp391/group6/controller/DemoServlet.java
+++ src/main/java/com/swp391/group6/controller/DemoServlet.java
@@ -1,15 +1,14 @@
 package com.swp391.group6.controller;
+
 import com.swp391.group6.util.DBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @WebServlet(name = "DemoServlet", urlPatterns = {"/demo"})
 public class DemoServlet extends HttpServlet {
@@ -17,23 +16,19 @@ public class DemoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        try {
-            String statusMessage;
-            String cssClass;
-            Connection conn = DBContext.getConnection();
-            if (conn != null) {
-                statusMessage = "SUCCESS: Database Connected via XAMPP MySQL!";
-                cssClass = "success";
-            } else {
-                statusMessage = "ERROR: Could not connect to Database. Check Console Logs.";
-                cssClass = "error";
-            }
-            
-            request.setAttribute("message", statusMessage);
-            request.setAttribute("statusClass", cssClass);
-            request.getRequestDispatcher("demo.jsp").forward(request, response);
-        } catch (SQLException ex) {
-            Logger.getLogger(DemoServlet.class.getName()).log(Level.SEVERE, null, ex);
+        String statusMessage;
+        String cssClass;
+        Connection conn = DBContext.getConnection();
+        if (conn != null) {
+            statusMessage = "SUCCESS: Database Connected via XAMPP MySQL!";
+            cssClass = "success";
+        } else {
+            statusMessage = "ERROR: Could not connect to Database. Check Console Logs.";
+            cssClass = "error";
         }
+
+        request.setAttribute("message", statusMessage);
+        request.setAttribute("statusClass", cssClass);
+        request.getRequestDispatcher("demo.jsp").forward(request, response);
     }
 }
